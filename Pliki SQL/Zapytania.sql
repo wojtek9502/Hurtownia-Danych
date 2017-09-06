74ba898403bb83e7d90f0c163b1efc34cc9ca24d
wojtek <wojtek.klusek2@gmail.com>
2017-09-06 20:46:52 +0200

    Queries final

--- Pliki SQL/Zapytania.sql
+++ Pliki SQL/Zapytania.sql
@@ -18,7 +18,7 @@ GROUP BY rollup (id_dnia, id_dentysty);
 SELECT  Nvl(To_Char(id_gabinetu),'SUMA GABINET') AS id_gabinetu,
         Nvl(To_Char(id_dentysty),'SUMA DENTYSTA') AS id_dentysty,
         Nvl(To_Char(nazwa_zabiegu),'WSZYSTKICH ZABIEGOW') AS  nazwa_zabiegu,
-        Count(id_zabiegu) AS ilosc_obsluzonych
+        Count(id_zabiegu) AS ilosc_zabiegow
         FROM zabieg
 GROUP BY cube(id_gabinetu,id_dentysty,nazwa_zabiegu)
 ORDER BY id_gabinetu asc,id_dentysty asc,nazwa_zabiegu ASC;
@@ -31,12 +31,58 @@ SELECT Nvl(To_Char(id_gabinetu),'SUMA GABINET') AS id_gabinetu,
        FROM zabieg
 GROUP BY cube(id_gabinetu,nazwa_zabiegu);
 
+---grupping sets
+---ile w danym dniu bylo zabiegow w kazdym z gabinetow
+SELECT  Nvl(To_Char(id_gabinetu),'SUMA GABINET') AS id_gabinetu,
+        Nvl(To_Char(id_dnia),'SUMA DNIA') AS id_dnia,
+        Sum(cena) AS suma_zysku
+       FROM zabieg
+GROUP BY Grouping sets ((id_gabinetu,id_dnia), id_dnia ,())
+ORDER BY id_gabinetu desc, id_dnia;
 
 
-SELECT Count(id_dentysty) FROM dentysta;
+---ile rodzajow zabiegow wykonal dany dentyska w zaleznosci od dnia
+SELECT Nvl(To_Char(id_dentysty),'SUMA DENTYSTA') AS id_dentysty,
+       Nvl(To_Char(id_dnia),'SUMA DNIA') AS id_dnia,
+       Nvl(To_Char(nazwa_zabiegu),'WSZYSTKICH ZABIEGOW') AS  nazwa_zabiegu,
+       Count(id_zabiegu) AS liczba_zabiegow
+       FROM zabieg
+GROUP BY Grouping sets ((id_dentysty,id_dnia,nazwa_zabiegu), id_dnia ,())
+ORDER BY id_dentysty desc , id_dnia;
+
+
+---partition
+SELECT distinct id_gabinetu, id_dnia,
+Sum(cena) OVER(
+                    PARTITION BY id_gabinetu
+                    ORDER BY id_dnia
+                    RANGE BETWEEN unbounded preceding  AND CURRENT ROW
+                    ) AS zarobek
+FROM zabieg
+WHERE id_dnia <29
+ORDER BY id_gabinetu;
 
 
 
+SELECT distinct id_gabinetu, id_dnia,
+Sum(cena) OVER(
+                    PARTITION BY id_gabinetu
+                    ORDER BY id_dnia
+                    RANGE BETWEEN unbounded preceding  AND CURRENT ROW
+                    ) AS zarobek
+FROM zabieg
+WHERE id_dnia <29
+ORDER BY id_gabinetu;
+
+
+SELECT distinct id_gabinetu, id_dentysty, id_dnia,
+Count(id_zabiegu) OVER(
+                    PARTITION BY id_gabinetu
+                    ORDER BY id_dnia,id_dentysty
+                    RANGE BETWEEN unbounded preceding  AND CURRENT ROW
+                    ) AS liczba_sprzedanych_pism
+FROM zabieg
+ORDER BY id_gabinetu,id_dentysty;
 
 
 ---ktory dentysta zarobil najwiecej

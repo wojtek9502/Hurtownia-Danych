4f6e80f75be09c2d62ea02d66a876f783af58fcd
wojtek <wojtek.klusek2@gmail.com>
2017-09-08 16:40:04 +0200

    Update

--- Pliki SQL/Zapytania.sql
+++ Pliki SQL/Zapytania.sql
@@ -34,7 +34,7 @@ GROUP BY cube(id_gabinetu,nazwa_zabiegu);
 ---grupping sets
 ---ile w danym dniu bylo zabiegow w kazdym z gabinetow
 SELECT  Nvl(To_Char(id_gabinetu),'SUMA GABINET') AS id_gabinetu,
-        Nvl(To_Char(id_dnia),'SUMA DNIA') AS id_dnia,
+        Nvl(To_Char(id_dnia),'SUMA DNI') AS id_dnia,
         Sum(cena) AS suma_zysku
        FROM zabieg
 GROUP BY Grouping sets ((id_gabinetu,id_dnia), id_dnia ,())
@@ -43,11 +43,11 @@ ORDER BY id_gabinetu desc, id_dnia;
 
 ---ile rodzajow zabiegow wykonal dany dentyska w zaleznosci od dnia
 SELECT Nvl(To_Char(id_dentysty),'SUMA DENTYSTA') AS id_dentysty,
-       Nvl(To_Char(id_dnia),'SUMA DNIA') AS id_dnia,
+       Nvl(To_Char(id_dnia),'SUMA DNI') AS id_dnia,
        Nvl(To_Char(nazwa_zabiegu),'WSZYSTKICH ZABIEGOW') AS  nazwa_zabiegu,
        Count(id_zabiegu) AS liczba_zabiegow
        FROM zabieg
-GROUP BY Grouping sets ((id_dentysty,id_dnia,nazwa_zabiegu), id_dnia ,())
+GROUP BY Grouping sets ((id_dentysty,id_dnia,nazwa_zabiegu),(id_dentysty, nazwa_zabiegu), id_dnia ,())
 ORDER BY id_dentysty desc , id_dnia;
 
 
@@ -63,31 +63,19 @@ WHERE id_dnia <29
 ORDER BY id_gabinetu;
 
 
-
-SELECT distinct id_gabinetu, id_dnia,
-Sum(cena) OVER(
-                    PARTITION BY id_gabinetu
+SELECT distinct id_gabinetu, id_dentysty, id_pacjenta, id_dnia,
+Count(id_zabiegu) OVER(
+                    PARTITION BY id_gabinetu,id_dentysty
                     ORDER BY id_dnia
                     RANGE BETWEEN unbounded preceding  AND CURRENT ROW
-                    ) AS zarobek
-FROM zabieg
-WHERE id_dnia <29
-ORDER BY id_gabinetu;
-
-
-SELECT distinct id_gabinetu, id_dentysty, id_dnia,
-Count(id_zabiegu) OVER(
-                    PARTITION BY id_gabinetu
-                    ORDER BY id_dnia,id_dentysty
-                    RANGE BETWEEN unbounded preceding  AND CURRENT ROW
-                    ) AS liczba_sprzedanych_pism
+                    ) AS liczba_wykonanych_zabiegow
 FROM zabieg
 ORDER BY id_gabinetu,id_dentysty;
 
 
 ---ktory dentysta zarobil najwiecej
 SELECT id_dentysty,
-       SUM(cena) AS ilosc_zabiegow,
+       SUM() AS ilosc_zabiegow,
        Rank() OVER (PARTITION BY NULL ORDER BY Sum(cena) desc) AS ranking
 FROM zabieg
 GROUP BY id_dentysty;
